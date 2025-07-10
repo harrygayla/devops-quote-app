c1346295cf2e1c6a1e9ea7d60e5eecdc81531544
Harry Gayla <harrygayla@gmail.com>
2025-07-10 09:07:50 +0800

    code changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function getQuote() {
-  const response = await get("https://devops-quote-app-production.up.railway.app/api/quote"); // Replace with your Railway API URL
+  const response = await fetch("https://devops-quote-app-production.up.railway.app/api/quote"); // Replace with your Railway API URL
   const data = await response.json();
   document.getElementById("quote").innerText = data.quote;
 }
